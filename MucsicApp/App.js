854a68e8918592a0fe5c86bc130b918fe7782d25
Kiệt Trần <trantuankiet5980@gmail.com>
2024-10-27 15:17:10 +0700

    Header HomeAudioListing Screen

--- MucsicApp/App.js
+++ MucsicApp/App.js
@@ -1,17 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, TouchableOpacity, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import LaunchScreen from './src/pages/LaunchScreen';
+import HomeAudioListing from './src/pages/HomeAudioListing';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={LaunchScreen} options={{headerShown: false}}/>
-        
+        <Stack.Screen name="LaunchScreen" component={LaunchScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="HomeAudioListing" component={HomeAudioListing} options={({navigation}) => (
+          {
+            headerTitle: '',
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Image
+                  source={require('./assets/Home - Audio Listing/Image 36.png')}
+                  style={{ width: 30, height: 30, marginLeft: 10 }}
+                />
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <TouchableOpacity>
+                  <Icon name="bell" size={30} color="#000" style={{marginRight: 10}} />
+                </TouchableOpacity>
+                <TouchableOpacity style={{marginLeft: 10}}>
+                  <Image source={require('./assets/Home - Audio Listing/Avatar 3.png')} />
+                </TouchableOpacity>
+              </View>
+            ),
+            headerShadowVisible: false,
+          }
+        )} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- MucsicApp/src/pages/LaunchScreen.js
+++ MucsicApp/src/pages/LaunchScreen.js
@@ -1,7 +1,7 @@
 import {Text, View, Image, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function LaunchScreen() {
+export default function LaunchScreen({navigation}) {
     return (
         <SafeAreaView style={{flex: 1, alignItems: 'center'}}>
             <Image style={{width: '100%', height: 900, position: 'absolute', top: 0}} source={require('../../assets/LaunchScreen/Image30.png')} />
@@ -10,7 +10,7 @@ export default function LaunchScreen() {
             <Text style={{fontSize: 40, fontWeight: 'bold', color: '#fff', marginTop: 300}}>Your music</Text>
             <Text style={{fontSize: 40, fontWeight: 'bold', color: '#fff'}}>Your</Text>
             <Text style={{fontSize: 40, fontWeight: 'bold', color: '#fff'}}>artists</Text>
-            <TouchableOpacity style={{backgroundColor: '#000', width: '90%', alignItems: 'center', padding: 10, borderRadius: 30, marginTop: 100}}>
+            <TouchableOpacity onPress={() => navigation.navigate('HomeAudioListing')} style={{backgroundColor: '#000', width: '90%', alignItems: 'center', padding: 10, borderRadius: 30, marginTop: 100}}>
                 <Text style={{color: '#fff', fontSize: 20}}>Create an account</Text>
             </TouchableOpacity>
             <TouchableOpacity style={{backgroundColor: '#fff', width: '90%', alignItems: 'center', padding: 10, borderRadius: 30, marginTop: 20}}>
